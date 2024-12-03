d072b0ff62c78e071f926055dd0d7eee12a277f9
noelfoka <nomgnenoel@gmail.com>
2024-12-03 16:33:29 +0100

    update

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,12 +2,12 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchRevenue, fetchLatestInvoices, fetchCardData } from "@/app/lib/data";
+import { fetchLatestInvoices, fetchCardData } from "@/app/lib/data";
 
 export default async function page() {
 
   const latestInvoices = await fetchLatestInvoices();
-  const revenue = await fetchRevenue();
+  // const revenue = await fetchRevenue();
   const { totalPaidInvoices, totalPendingInvoices, numberOfInvoices, numberOfCustomers } = await fetchCardData();
 
   return (
@@ -26,7 +26,7 @@ export default async function page() {
         />
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
-        <RevenueChart revenue={revenue}  />
+        {/* <RevenueChart revenue={revenue}  /> */}
         <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
