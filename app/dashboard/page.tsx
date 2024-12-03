36131797c96495461e2f08bb7741eb917cfde8f3
noelfoka <nomgnenoel@gmail.com>
2024-12-03 12:19:08 +0100

    fetching latect-invoice

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -2,9 +2,11 @@ import { Card } from "@/app/ui/dashboard/cards";
 import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
-import { fetchRevenue } from "@/app/lib/data";
+import { fetchRevenue, fetchLatestInvoices } from "@/app/lib/data";
 
 export default async function page() {
+
+  const latestInvoices = await fetchLatestInvoices();
   const revenue = await fetchRevenue();
   return (
     <main>
@@ -23,7 +25,7 @@ export default async function page() {
       </div>
       <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
         <RevenueChart revenue={revenue}  />
-        {/* <LatestInvoices latestInvoices={latestInvoices} /> */}
+        <LatestInvoices latestInvoices={latestInvoices} />
       </div>
     </main>
   );
