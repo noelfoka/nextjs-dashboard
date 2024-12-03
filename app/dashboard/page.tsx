d07845ac309d2835a037204aa7c4b145f8ddcdca
noelfoka <nomgnenoel@gmail.com>
2024-12-03 12:01:15 +0100

    update

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,8 @@
+import { Card } from "@/app/ui/dashboard/cards";
+import RevenueChart from "@/app/ui/dashboard/revenue-chart";
+import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
+import { lusitana } from "@/app/ui/fonts";
+
 export default function page() {
   return (
     <div>
