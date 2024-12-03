d2e1459e8774b20007a89ce66172be1df9ace6e9
noelfoka <nomgnenoel@gmail.com>
2024-12-03 16:35:47 +0100

    import some component to page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -3,6 +3,8 @@ import RevenueChart from "@/app/ui/dashboard/revenue-chart";
 import LatestInvoices from "@/app/ui/dashboard/latest-invoices";
 import { lusitana } from "@/app/ui/fonts";
 import { fetchLatestInvoices, fetchCardData } from "@/app/lib/data";
+import { Suspense } from "react";
+import { RevenueChartSkeleton } from "@/app/ui/skeletons";
 
 export default async function page() {
 
